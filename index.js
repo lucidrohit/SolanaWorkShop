1f911e1bfe6347411dd916110f30d9fb77ff39bf
Rohit Gupta <lucidrohit@gmail.com>
2022-11-02 12:39:49 +0530

    cli input base

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-function stirngPalindrome(string){
-    const length = string.length
-    for (let i = 0; i < length/2; i++) {
-        if(string[i]!==string[length-i-1])
-        return false
-    }
-    return true
+
+function stirngPalindrome(string) {
+  const length = string.length;
+  for (let i = 0; i < length / 2; i++) {
+    if (string[i] !== string[length - i - 1]) return false;
+  }
+  return true;
 }
+
+
+console.log(stirngPalindrome(process.argv[2]));
