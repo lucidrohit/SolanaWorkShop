65a541520df4e957c59fa2613f8d0248fa56a088
Rohit Gupta <lucidrohit@gmail.com>
2022-11-02 10:55:00 +0530

    created string palindrome pgm

---
+++ index.js
@@ -0,0 +1,8 @@
+function stirngPalindrome(string){
+    const length = string.length
+    for (let i = 0; i < length/2; i++) {
+        if(string[i]!==string[length-i-1])
+        return false
+    }
+    return true
+}
